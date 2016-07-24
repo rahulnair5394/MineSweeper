fa72a36e79d5a40d272eb86d8f67bdef7bd8b328
Rahul Nair <rahul.nair@bitwiseglobal.com>
2016-07-24 17:45:15 +0530

    On a verge to complete

--- src/main/java/com/bitwise/kata/MineField.java
+++ src/main/java/com/bitwise/kata/MineField.java
@@ -4,50 +4,24 @@ package com.bitwise.kata;
 /**
  * Created by rahul.nair@bitwiseglobal.com .
  */
-public class MineField implements Field {
+public class MineField extends Field {
 
-    private final int noOfRows;
-    private final int noOfColumns;
-    private final Square[][] squares;
+
 
 
     public MineField(int noOfRows, int noOfColumns) {
-        this.noOfRows = noOfRows;
-        this.noOfColumns = noOfColumns;
-        squares = new Square[this.noOfRows][this.noOfColumns];
+        super(noOfRows,noOfColumns);
 
-    }
 
-    public int getNoOfRows() {
-        return noOfRows;
-    }
-
-    public int getNoOfColumns() {
-        return noOfColumns;
     }
 
     @Override
-    public void setSquares(String[] fieldSquaresInputString) {
-        validateColumnInputSquares(fieldSquaresInputString);
-        for (int i = 0; i < noOfRows; i++) {
-            fieldSquaresInputString[i] = fieldSquaresInputString[i].replaceAll("\\s+", "");
-            char[] currentRow = fieldSquaresInputString[i].toCharArray();
-            for (int j = 0; j < noOfColumns; j++) squares[i][j] = new Square(currentRow[j]);
-        }
-    }
+    void validateColumnInputSquares(String[] fieldSquaresInputString) {
 
-    @Override
-    public Square[][] getSquares() {
-       return squares;
-    }
+        for (int i = 0; i < this.getNoOfColumns(); i++) {
 
-
-    private void validateColumnInputSquares(String[] fieldSquaresInputString) {
-
-        for (int i = 0; i < noOfColumns; i++) {
-
-            if (fieldSquaresInputString.length != noOfRows ||
-                    fieldSquaresInputString[i].length() != noOfColumns ||
+            if (fieldSquaresInputString.length != this.getNoOfRows() ||
+                    fieldSquaresInputString[i].length() != this.getNoOfColumns() ||
                     !fieldSquaresInputString[i].matches("[*.\\s]+"))
                 throw new InputValidationFailedException();
         }
